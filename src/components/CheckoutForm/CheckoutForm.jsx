0b5fd37d9ffeab548c706902b13d91a3b044fb60
Sandra Alejandra <46465133+SandraGro@users.noreply.github.com>
2024-08-07 00:10:17 -0600

    WIP

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import {useState} from 'react'
+import { Checkout } from '../Checkout/Checkout';
 
-export default function CheckoutForm() {
-  return (
-    <div>CheckoutForm</div>
-  )
+const CheckoutForm =({onConfirm}) => {
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [ email, setEmail] = useState ('');
+
+    const handleconfirm = (event) => {
+        event.preventDefault()
+
+        const userData = {
+            name, phone, email
+        }
+
+        onConfirm(userData);
+    }
+    return (
+        <div className='container'>
+            <form onSubmit={handleconfirm} className='form'>
+                <label className='label'>
+                    Name:
+                </label>
+                <input className='input' type='text' value={phone} onChange={({target}) => setPhone(target.value)}/>
+            </form>
+        </div>
+    )
 }
+
+export default CheckoutForm;
